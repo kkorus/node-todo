71f77b987ad64217c77fab9b0fd415d1367cef34
Konrad Korus <konrad.korus@massive.co>
2017-09-30 18:02:23 +0100

    Added Api Controller

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var index = require('./config');
 var setupController = require('./controllers/setupController');
+var apiController = require('./controllers/apiController');
 var app = express();
 
 var port = process.env.PORT || 3000;
@@ -11,5 +12,6 @@ app.set('view enginge', 'ejs');
 
 mongoose.connect(index.getDbConnectionString());
 setupController(app);
+apiController(app);
 
 app.listen(port);
